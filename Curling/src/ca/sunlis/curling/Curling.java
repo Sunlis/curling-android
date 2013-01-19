7342df296af43fc50344845386219bc0c7c82087
Sean Dohring <sean@sunlis.ca>
2013-01-19 17:15:30 -0500

    Use box2d for physics. Give each stone its own box2d circle to follow.

--- Curling/src/ca/sunlis/curling/Curling.java
+++ Curling/src/ca/sunlis/curling/Curling.java
@@ -9,16 +9,38 @@ import com.badlogic.gdx.graphics.GL10;
 import com.badlogic.gdx.graphics.OrthographicCamera;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.math.Vector2;
+import com.badlogic.gdx.physics.box2d.Body;
+import com.badlogic.gdx.physics.box2d.BodyDef;
+import com.badlogic.gdx.physics.box2d.Box2DDebugRenderer;
+import com.badlogic.gdx.physics.box2d.CircleShape;
+import com.badlogic.gdx.physics.box2d.Fixture;
+import com.badlogic.gdx.physics.box2d.FixtureDef;
+import com.badlogic.gdx.physics.box2d.PolygonShape;
+import com.badlogic.gdx.physics.box2d.World;
+import com.badlogic.gdx.physics.box2d.BodyDef.BodyType;
 
 public class Curling implements ApplicationListener {
 	static OrthographicCamera camera;
 	static SpriteBatch batch;
-	static Texture redRock;
-	static Texture blueRock;
-	static List<Stone> stones;
-	
 	static float screenW;
 	static float screenH;
+	
+	//Box2D
+	static World world;
+	Box2DDebugRenderer debugRenderer;
+	static final float BOX_STEP = 1/60f;
+    static final int BOX_VELOCITY_ITERATIONS = 6;
+    static final int BOX_POSITION_ITERATIONS = 2;
+    static final float WORLD_TO_BOX = 1/55; //TODO
+    static final float BOX_TO_WORLD = 55; //TODO
+	
+	// Textures
+	static Texture redRock;
+	static Texture blueRock;
+	
+	// Stones
+	static List<Stone> stones;
     
     static boolean mouseDown = false;
     
@@ -33,15 +55,34 @@ public class Curling implements ApplicationListener {
 		camera.setToOrtho(false, screenW, screenH);
 		batch = new SpriteBatch();
 		
+		world = new World(new Vector2(100,100), true);
+		debugRenderer = new Box2DDebugRenderer();
+		
+		createScreenBox();
+        
+        //Dynamic Body
+        BodyDef bodyDef = new BodyDef();
+        bodyDef.type = BodyType.DynamicBody;
+        bodyDef.position.set(camera.viewportWidth / 2, camera.viewportHeight / 2);
+        Body body = world.createBody(bodyDef);
+        CircleShape dynamicCircle = new CircleShape();
+        dynamicCircle.setRadius(1*BOX_TO_WORLD);
+        FixtureDef fixtureDef = new FixtureDef();
+        fixtureDef.shape = dynamicCircle;
+        fixtureDef.density = 1.0f;
+        fixtureDef.friction = 0.5f;
+        fixtureDef.restitution = 1.0f;
+        body.createFixture(fixtureDef);
+		
 		redRock = new Texture(Gdx.files.internal("redRock.png"));
 		blueRock = new Texture(Gdx.files.internal("blueRock.png"));
 		
 		stones = new ArrayList<Stone>();
-		for(int i = 0; i < 3; i++) {
+		for(int i = 0; i < 4; i++) {
 			stones.add(new Stone(((i%4)+1) * (screenW/5), (float) ((Math.floor(i/4)+1) * (screenH/5)), i%2 == 0));
 		}
 		
-		Gdx.graphics.setContinuousRendering(false);
+//		Gdx.graphics.setContinuousRendering(false);
 	    Gdx.graphics.requestRendering();
 	}
 
@@ -55,14 +96,6 @@ public class Curling implements ApplicationListener {
 		Gdx.gl.glClearColor(1, 1, 1, 1);
 		Gdx.gl.glClear(GL10.GL_COLOR_BUFFER_BIT);
 		camera.update();
-		
-		for (int i = 0; i < stones.size(); i++) {
-			for (int j = i+1; j < stones.size(); j++) {
-				if (stones.get(i).colliding(stones.get(j))) {
-					stones.get(i).handleCollision(stones.get(j));
-				}
-			}
-		}
 		
 		if (!Gdx.input.isTouched() && mouseDown) {
 			 for (Stone stone : stones) {
@@ -79,13 +112,16 @@ public class Curling implements ApplicationListener {
 		if (dontNeedRender != stones.size() || mouseDown) {
 			Gdx.graphics.requestRendering();
 		} else {
-			Gdx.gl.glClearColor(0, 1, 1, 1);
+			Gdx.gl.glClearColor(0.9f, 1, 1, 1);
 			Gdx.gl.glClear(GL10.GL_COLOR_BUFFER_BIT);
 			camera.update();
 			for (Stone stone : stones) {
 				stone.draw();
 			}
 		}
+		
+		debugRenderer.render(world, camera.combined);  
+        world.step(BOX_STEP, BOX_VELOCITY_ITERATIONS, BOX_POSITION_ITERATIONS);
 	}
 
 	@Override
@@ -98,5 +134,36 @@ public class Curling implements ApplicationListener {
 
 	@Override
 	public void resume() {
+	}
+
+	private void createScreenBox() {
+		//bottom
+        BodyDef groundBodyDef=new BodyDef();
+        groundBodyDef.position.set(new Vector2(0, 0));
+        Body groundBody = world.createBody(groundBodyDef);
+        PolygonShape groundBox = new PolygonShape();
+        groundBox.setAsBox(camera.viewportWidth, 0);
+        groundBody.createFixture(groundBox, 0.0f);
+        //top
+        BodyDef topBodyDef=new BodyDef();
+        topBodyDef.position.set(new Vector2(0, camera.viewportHeight));
+        Body topBody = world.createBody(topBodyDef);
+        PolygonShape topBox = new PolygonShape();
+        topBox.setAsBox(camera.viewportWidth, 0);
+        topBody.createFixture(topBox, 0.0f);
+        // left
+        BodyDef leftBodyDef=new BodyDef();
+        leftBodyDef.position.set(new Vector2(0, 0));
+        Body leftBody = world.createBody(leftBodyDef);
+        PolygonShape leftBox = new PolygonShape();
+        leftBox.setAsBox(0, camera.viewportHeight);
+        leftBody.createFixture(leftBox, 0.0f);
+        // right
+        BodyDef rightBodyDef=new BodyDef();
+        rightBodyDef.position.set(new Vector2(camera.viewportWidth, 0));
+        Body rightBody = world.createBody(rightBodyDef);
+        PolygonShape rightBox = new PolygonShape();
+        rightBox.setAsBox(0, camera.viewportHeight);
+        rightBody.createFixture(rightBox, 0.0f);
 	}
 }
--- Curling/src/ca/sunlis/curling/Stone.java
+++ Curling/src/ca/sunlis/curling/Stone.java
@@ -3,6 +3,12 @@ package ca.sunlis.curling;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.math.Circle;
 import com.badlogic.gdx.math.Vector2;
+import com.badlogic.gdx.physics.box2d.Body;
+import com.badlogic.gdx.physics.box2d.BodyDef;
+import com.badlogic.gdx.physics.box2d.BodyDef.BodyType;
+import com.badlogic.gdx.physics.box2d.CircleShape;
+import com.badlogic.gdx.physics.box2d.Fixture;
+import com.badlogic.gdx.physics.box2d.FixtureDef;
 
 public class Stone {
 	static double mass = 18; //(kg)
@@ -52,76 +58,6 @@ public class Stone {
 		if (this.velX == 0 && this.velY == 0) {
 			Curling.dontNeedRender++;
 		}
-	}
-	
-	public boolean colliding(Stone that) {
-	    float xd = this.circle.x - that.circle.x;
-	    float yd = this.circle.y - that.circle.y;
-
-	    float sumRadius = this.circle.radius + that.circle.radius;
-	    float sqrRadius = sumRadius * sumRadius;
-
-	    float distSqr = (xd * xd) + (yd * yd);
-
-	    return (distSqr <= sqrRadius);
-	}
-	
-	public void handleCollision(Stone that) {
-		// credit to http://stackoverflow.com/questions/345838/ball-to-ball-collision-detection-and-handling for collision handling
-		
-		Vector2 position = new Vector2();
-		position.x = this.circle.x;
-		position.y = this.circle.y;
-		Vector2 thatPosition = new Vector2();
-		thatPosition.x = that.circle.x;
-		thatPosition.y = that.circle.y;
-		
-		Vector2 velocity = new Vector2();
-		velocity.x = (float) this.velX;
-		velocity.y = (float) this.velY;
-		Vector2 thatVelocity = new Vector2();
-		thatVelocity.x = (float) that.velX;
-		thatVelocity.y = (float) that.velY;
-		
-		// get the mtd
-		Vector2 delta = new Vector2(position).sub(thatPosition);
-		double d = delta.len();
-		
-		// minimum translation distance to push balls apart after intersecting
-		Vector2 mtd = new Vector2(delta).mul((float) (((this.circle.radius + that.circle.radius)-d)/d)); 
-		
-		
-		// resolve intersection --
-		// inverse mass quantities
-		float im1 = (float) (1 / Stone.mass); 
-		float im2 = (float) (1 / Stone.mass);
-		
-		// push-pull them apart based off their mass
-		position.add(new Vector2(mtd).mul(im1 / (im1 + im2)));
-		thatPosition.sub(new Vector2(mtd).mul(im2 / (im1 + im2)));
-		
-		// impact speed
-		Vector2 v = new Vector2(velocity).sub(thatVelocity);
-		float vn = v.dot(new Vector2(mtd).nor());
-		
-		// sphere intersecting but moving away from each other already
-		if (vn > 0.0f) return;
-		// collision impulse
-		float i = (float) ((-(1.0f + 0.95) * vn) / (im1 + im2));
-		Vector2 impulse = new Vector2(mtd).mul(i);
-		
-		// change in momentum
-		velocity.add(new Vector2(impulse).mul(im1));
-		thatVelocity.sub(new Vector2(impulse).mul(im2));
-		
-		this.circle.x = position.x;
-		this.circle.y = position.y;
-		this.velX = velocity.x;
-		this.velY = velocity.y;
-		that.circle.x = thatPosition.x;
-		that.circle.y = thatPosition.y;
-		that.velX = thatVelocity.x;
-		that.velY = thatVelocity.y;
 	}
 	
 	public double futureX() {
