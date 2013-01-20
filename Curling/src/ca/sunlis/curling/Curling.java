1ab269d72edb0979e318f83327eb562faed7b9b2
Sean Dohring <sean@sunlis.ca>
2013-01-19 23:38:02 -0500

    Switch to Box2D for basic stone movements and collisions
    TODO: Add friction to general movement as well as collisions (static friction)

--- Curling/src/ca/sunlis/curling/Curling.java
+++ Curling/src/ca/sunlis/curling/Curling.java
@@ -1,15 +1,21 @@
 package ca.sunlis.curling;
 
 import java.util.ArrayList;
+import java.util.Iterator;
 import java.util.List;
 
 import com.badlogic.gdx.ApplicationListener;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.GL10;
 import com.badlogic.gdx.graphics.OrthographicCamera;
+import com.badlogic.gdx.graphics.Pixmap;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.graphics.g2d.TextureRegion;
+import com.badlogic.gdx.graphics.glutils.ShapeRenderer;
+import com.badlogic.gdx.graphics.glutils.ShapeRenderer.ShapeType;
 import com.badlogic.gdx.math.Vector2;
+import com.badlogic.gdx.math.Vector3;
 import com.badlogic.gdx.physics.box2d.Body;
 import com.badlogic.gdx.physics.box2d.BodyDef;
 import com.badlogic.gdx.physics.box2d.Box2DDebugRenderer;
@@ -17,6 +23,7 @@ import com.badlogic.gdx.physics.box2d.CircleShape;
 import com.badlogic.gdx.physics.box2d.Fixture;
 import com.badlogic.gdx.physics.box2d.FixtureDef;
 import com.badlogic.gdx.physics.box2d.PolygonShape;
+import com.badlogic.gdx.physics.box2d.QueryCallback;
 import com.badlogic.gdx.physics.box2d.World;
 import com.badlogic.gdx.physics.box2d.BodyDef.BodyType;
 
@@ -26,14 +33,18 @@ public class Curling implements ApplicationListener {
 	static float screenW;
 	static float screenH;
 	
+	ShapeRenderer shapeRenderer;
+	
 	//Box2D
 	static World world;
 	Box2DDebugRenderer debugRenderer;
+	Body hitBody;
+	static float TOUCH_OFFSET = 26f;
 	static final float BOX_STEP = 1/60f;
     static final int BOX_VELOCITY_ITERATIONS = 6;
     static final int BOX_POSITION_ITERATIONS = 2;
-    static final float WORLD_TO_BOX = 1/55; //TODO
-    static final float BOX_TO_WORLD = 55; //TODO
+    static final float BOX_TO_WORLD = 26; //TODO
+    static final float WORLD_TO_BOX = 1/BOX_TO_WORLD; //TODO
 	
 	// Textures
 	static Texture redRock;
@@ -43,6 +54,7 @@ public class Curling implements ApplicationListener {
 	static List<Stone> stones;
     
     static boolean mouseDown = false;
+    Vector2 touchPoint;
     
 	public static int dontNeedRender;
 	
@@ -55,73 +67,96 @@ public class Curling implements ApplicationListener {
 		camera.setToOrtho(false, screenW, screenH);
 		batch = new SpriteBatch();
 		
-		world = new World(new Vector2(100,100), true);
+		world = new World(new Vector2(0,0), true);
 		debugRenderer = new Box2DDebugRenderer();
+		
+		shapeRenderer = new ShapeRenderer();
 		
 		createScreenBox();
         
-        //Dynamic Body
-        BodyDef bodyDef = new BodyDef();
-        bodyDef.type = BodyType.DynamicBody;
-        bodyDef.position.set(camera.viewportWidth / 2, camera.viewportHeight / 2);
-        Body body = world.createBody(bodyDef);
-        CircleShape dynamicCircle = new CircleShape();
-        dynamicCircle.setRadius(1*BOX_TO_WORLD);
-        FixtureDef fixtureDef = new FixtureDef();
-        fixtureDef.shape = dynamicCircle;
-        fixtureDef.density = 1.0f;
-        fixtureDef.friction = 0.5f;
-        fixtureDef.restitution = 1.0f;
-        body.createFixture(fixtureDef);
+//		createBody(camera.viewportWidth / 2, camera.viewportHeight / 2);
 		
 		redRock = new Texture(Gdx.files.internal("redRock.png"));
 		blueRock = new Texture(Gdx.files.internal("blueRock.png"));
 		
 		stones = new ArrayList<Stone>();
-		for(int i = 0; i < 4; i++) {
-			stones.add(new Stone(((i%4)+1) * (screenW/5), (float) ((Math.floor(i/4)+1) * (screenH/5)), i%2 == 0));
+		for(int i = 0; i < 16; i++) {
+			float x = ((i%4)+1) * (screenW/5);
+			float y = (float) ((Math.floor(i/4)+1) * (screenH/5));
+			stones.add(new Stone(x, y, i%2 == 0));
 		}
 		
-//		Gdx.graphics.setContinuousRendering(false);
+		Gdx.graphics.setContinuousRendering(false);
 	    Gdx.graphics.requestRendering();
 	}
 
 	@Override
 	public void dispose() {
 		batch.dispose();
+		debugRenderer.dispose();
 	}
 
 	@Override
 	public void render() {
+		update();
+		
 		Gdx.gl.glClearColor(1, 1, 1, 1);
 		Gdx.gl.glClear(GL10.GL_COLOR_BUFFER_BIT);
 		camera.update();
 		
-		if (!Gdx.input.isTouched() && mouseDown) {
-			 for (Stone stone : stones) {
-				 stone.ranDir();
-			 }
+		if (Gdx.input.isTouched() && hitBody != null) {
+			shapeRenderer.begin(ShapeType.Line);
+			shapeRenderer.setColor(0, 1, 0, 1);
+			shapeRenderer.line(touchPoint.x, touchPoint.y, Gdx.input.getX(), screenH-Gdx.input.getY());
+			shapeRenderer.end();
 		}
-		mouseDown = Gdx.input.isTouched();
 		
-		dontNeedRender = 0;
+		batch.setProjectionMatrix(camera.combined);
+		batch.begin();
+		
+		boolean wantRender = false;
 		for (Stone stone : stones) {
 			stone.draw();
+			if (!wantRender && stone.moving) wantRender = true;
 		}
 		
-		if (dontNeedRender != stones.size() || mouseDown) {
-			Gdx.graphics.requestRendering();
-		} else {
-			Gdx.gl.glClearColor(0.9f, 1, 1, 1);
-			Gdx.gl.glClear(GL10.GL_COLOR_BUFFER_BIT);
-			camera.update();
-			for (Stone stone : stones) {
-				stone.draw();
-			}
-		}
 		
+		batch.end();
 		debugRenderer.render(world, camera.combined);  
         world.step(BOX_STEP, BOX_VELOCITY_ITERATIONS, BOX_POSITION_ITERATIONS);
+		
+		if (wantRender) Gdx.graphics.requestRendering();
+	}
+	
+	public void update() {
+		if (Gdx.input.isTouched() && !mouseDown) {
+			// mouse just pressed
+			touchPoint = new Vector2(Gdx.input.getX(), screenH-Gdx.input.getY());
+			hitBody = null;
+			Iterator<Body> bodyIterator = world.getBodies();
+			while (bodyIterator.hasNext()) {
+				Body body = bodyIterator.next();
+				if (body.getType() != BodyType.DynamicBody) continue;
+				if (body.getPosition().sub(touchPoint).len() < TOUCH_OFFSET) {
+					hitBody = body;
+					break;
+				}
+			}
+			if (hitBody != null) {
+				hitBody.setTransform(touchPoint, 0f);
+				hitBody.setLinearVelocity(0,0);
+				hitBody.setAngularVelocity(0f);
+				hitBody.setType(BodyType.StaticBody);
+			}
+		} else if (Gdx.input.isTouched() && mouseDown && hitBody != null) {
+			// mouse down, was down previously
+		} else if (!Gdx.input.isTouched() && mouseDown && hitBody != null) {
+			// mouse just released
+			hitBody.setType(BodyType.DynamicBody);
+			Vector2 vel = new Vector2(Gdx.input.getX(), screenH-Gdx.input.getY()).sub(touchPoint);
+			hitBody.setLinearVelocity(vel);
+		}
+		mouseDown = Gdx.input.isTouched();
 	}
 
 	@Override
@@ -135,11 +170,28 @@ public class Curling implements ApplicationListener {
 	@Override
 	public void resume() {
 	}
+	
+	public static Body createBody(float x, float y) {
+		//Dynamic Body
+        BodyDef bodyDef = new BodyDef();
+        bodyDef.type = BodyType.DynamicBody;
+		bodyDef.position.set(x, y);
+        Body body = world.createBody(bodyDef);
+        CircleShape dynamicCircle = new CircleShape();
+        dynamicCircle.setRadius(BOX_TO_WORLD);
+        FixtureDef fixtureDef = new FixtureDef();
+        fixtureDef.shape = dynamicCircle;
+        fixtureDef.density = 23f;
+        fixtureDef.friction = 0.5f;
+        fixtureDef.restitution = 1.0f;
+        body.createFixture(fixtureDef);
+        return body;
+	}
 
 	private void createScreenBox() {
 		//bottom
         BodyDef groundBodyDef=new BodyDef();
-        groundBodyDef.position.set(new Vector2(0, 0));
+        groundBodyDef.position.set(new Vector2(0, -1));
         Body groundBody = world.createBody(groundBodyDef);
         PolygonShape groundBox = new PolygonShape();
         groundBox.setAsBox(camera.viewportWidth, 0);
@@ -160,7 +212,7 @@ public class Curling implements ApplicationListener {
         leftBody.createFixture(leftBox, 0.0f);
         // right
         BodyDef rightBodyDef=new BodyDef();
-        rightBodyDef.position.set(new Vector2(camera.viewportWidth, 0));
+        rightBodyDef.position.set(new Vector2(camera.viewportWidth+1, 0));
         Body rightBody = world.createBody(rightBodyDef);
         PolygonShape rightBox = new PolygonShape();
         rightBox.setAsBox(0, camera.viewportHeight);
--- Curling/src/ca/sunlis/curling/Stone.java
+++ Curling/src/ca/sunlis/curling/Stone.java
@@ -1,6 +1,7 @@
 package ca.sunlis.curling;
 
 import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.Sprite;
 import com.badlogic.gdx.math.Circle;
 import com.badlogic.gdx.math.Vector2;
 import com.badlogic.gdx.physics.box2d.Body;
@@ -11,69 +12,31 @@ import com.badlogic.gdx.physics.box2d.Fixture;
 import com.badlogic.gdx.physics.box2d.FixtureDef;
 
 public class Stone {
-	static double mass = 18; //(kg)
-	Circle circle;
+	static float radius = 26;
 	Texture tex;
-	double velX = 0;
-	double velY = 0;
+	Sprite sprite;
+	Body body;
+	boolean moving = false;
 	
 	public Stone(float x, float y, boolean red) {
 		this.tex = red ? Curling.redRock : Curling.blueRock;
-		
-		this.circle = new Circle();
-		this.circle.radius = 26;
-		this.circle.x = x;
-		this.circle.y = y;
-		
-		this.ranDir();
-	}
-	
-	public void ranDir() {
-		int amplitude = 10;
-		this.velX = (Math.random()*amplitude) - (amplitude/2);
-		this.velY = (Math.random()*amplitude) - (amplitude/2);
+		sprite = new Sprite(this.tex);
+		this.body = Curling.createBody(x,y);
 	}
 	
 	public void update() {
-		// Screen edges
-		if (this.circle.x + this.velX + this.circle.radius >= Curling.screenW || this.circle.x + this.velX - this.circle.radius < 0) {
-			this.velX *= -1;
-		}
-		if (this.circle.y + this.velY + this.circle.radius >= Curling.screenH || this.circle.y + this.velY - this.circle.radius < 0) {
-			this.velY *= -1;
-		}
-		
-		this.circle.x += this.velX;
-		this.circle.y += this.velY;
-		
-		// TODO: Friction! =D
-		this.velX *= 0.98;
-		this.velY *= 0.98;
-		if (Math.abs(this.velX) < 0.02 && Math.abs(this.velY) < 0.02) {
-			this.velX = 0;
-			this.velY = 0;
-		}
-		
-		
-		if (this.velX == 0 && this.velY == 0) {
-			Curling.dontNeedRender++;
-		}
-	}
-	
-	public double futureX() {
-		return this.circle.x + this.velX;
-	}
-	public double futureY() {
-		return this.circle.y + this.velY;
+		sprite.setPosition(this.body.getPosition().x - Stone.radius, this.body.getPosition().y - Stone.radius);
+		sprite.setRotation((float) ((this.body.getAngle() * 180) / Math.PI));
+		this.moving = !this.body.getLinearVelocity().epsilonEquals(new Vector2(), 0.05f);
 	}
 	
 	public void draw() {
 		this.update();
 		
-		Curling.batch.setProjectionMatrix(Curling.camera.combined);
-		Curling.batch.begin();
-		Curling.batch.draw(this.tex, this.circle.x - this.circle.radius, this.circle.y - this.circle.radius);
-		Curling.batch.end();
+//		Curling.batch.setProjectionMatrix(Curling.camera.combined);
+//		Curling.batch.begin();
+		sprite.draw(Curling.batch);
+//		Curling.batch.end();
 	}
 	
 }
